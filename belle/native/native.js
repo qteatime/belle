2c06420c212f1a2a8e173e0e29771874cc21a735
Q <hello@qteati.me>
2021-08-22 01:34:28 +0200

    Initial work on web-based IDE

--- belle/native/native.js
+++ belle/native/native.js
@@ -10,5 +10,27 @@ exports.default = (ffi) => {
     return e;
   }
 
-  ffi.defun("belle.make-canvas", (element) => {});
+  ffi.defun("belle.box", (tag) => {
+    return ffi.box(document.createElement(ffi.text_to_string(tag)));
+  });
+
+  ffi.defun("belle.text", (content) => {
+    return ffi.box(document.createTextNode(ffi.text_to_string(content)));
+  });
+
+  ffi.defun("belle.append", (box, child) => {
+    ffi.unbox(box).append(ffi.unbox(child));
+    return box;
+  });
+
+  ffi.defun("belle.set-attribute", (box, name, value) => {
+    ffi
+      .unbox(box)
+      .setAttribute(ffi.text_to_string(name), ffi.text_to_string(value));
+    return box;
+  });
+
+  ffi.defun("belle.get-attribute", (box, name) => {
+    return ffi.text(ffi.unbox(box).getAttribute(ffi.text_to_string(name)));
+  });
 };
